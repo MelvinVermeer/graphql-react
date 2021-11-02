566bf27c358c116c2c23df7466376307853a465a
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-11-02 13:00:24 +0100

    typed useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
 import { gql, useQuery } from "@apollo/client";
 import "./App.css";
 import ProductCard from "./ProductCard";
+import { Product } from "./__generated__/gateway";
 
+// inspect the schema on https://studio.apollographql.com/sandbox/schema/sdl
 const GET_PRODUCT = gql`
   query product($productId: String!) {
     product(id: $productId) {
       id
       name
-      price
+      price # comment this field to show the issue
     }
   }
 `;
 
 function App() {
+  // As a first step we could provide a Type Argument <{ product: Product }>
   const { loading, error, data } = useQuery(GET_PRODUCT, {
-    variables: { productId: "25" },
+    variables: { productId: "25" }, // A typo here is easily made..
   });
 
   if (loading) return <p>Loading ⏳</p>;
   if (error) return <p>Error 😢</p>;
+  if (!data) return <p>No data 😢</p>;
 
   return (
     <div style={{ marginLeft: "20px" }}>
       <h4>Returned json object</h4>
       <pre>{JSON.stringify(data.product, null, 2)}</pre>
 
-      {/* Let's use this data in our component */}
       <ProductCard name={data.product.name} price={data.product.price} />
     </div>
   );
