1778693d02f65ada2eb5b990c2f681f23ffff4aa
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-11-02 12:45:57 +0100

    fetch data from GraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
+import { gql, useQuery } from "@apollo/client";
 import "./App.css";
 
+const GET_PRODUCT = gql`
+  query product($productId: String!) {
+    product(id: $productId) {
+      id
+      name
+      price
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(GET_PRODUCT, {
+    variables: { productId: "25" },
+  });
+
+  if (loading) return <p>Loading ⏳</p>;
+  if (error) return <p>Error 😢</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginLeft: "20px" }}>
+      <h4>Returned json object</h4>
+      <pre>{JSON.stringify(data.product, null, 2)}</pre>
     </div>
   );
 }
